978c07dcf691a3d7d286e967ce2876e247783fb6
Shiva Sunchu <chhotahathi06@gmail.com>
2023-01-22 19:25:03 +0530

    Adding Day-4 files

--- industry-lab/src/Pages/SigninPage.jsx
+++ industry-lab/src/Pages/SigninPage.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
+const formState = {
+  name: "",
+  email: "",
+  password: "",
+};
 export default function SigninPage() {
+  const [data, setData] = useState(formState);
+  const inputHandler = (e) => {
+    const value = e.target.value;
+    setData({ ...data, [e.target.name]: value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (data.name !== "" && data.email !== "" && data.password !== "") {
+      console.log(data);
+    }
+  };
   return (
     <div className="SigninPage">
-      <h1>Sign-in Page</h1>
+      <h1>Enter Your Credentials</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={(e) => inputHandler(e)}
+        />
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          onChange={(e) => inputHandler(e)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={(e) => inputHandler(e)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
